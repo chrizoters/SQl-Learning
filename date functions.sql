bcd71b54212373b08aa2bdecf4c562dbb05bc8c7
christopherprem <christopher.s@healthicity.com>
2017-12-14 13:02:07 +0530

    change

--- date functions.sql
+++ date functions.sql
@@ -1,8 +1,18 @@
  
  -- Date functions
 
+ /*  GETDATE() and SYSDATETIME()
+SQL Server uses GETDATE() and SYSDATETIME() to get a current date and time. They are nondeterministic functions: 
+views and expressions that reference these columns can’t be indexed. Both functions accept no arguments and 
+return the local current date and time. 
+The difference is, when we use
+ GETDATE(), the precision is to the milliseconds and 
+ in the case of SYSDATETIME(), the precision is to the nanoseconds. */
+
+
  --current Date
      select GETDATE() -- 2017-12-13 06:40:01.290
+	  select SYSDATETIME()  --2017-12-14 00:29:24.9350104
 
 --DateDIFF - DATEDIFF(datepart, date1, date2)
 			-----------------------------------
@@ -60,3 +70,5 @@
     select year (@dt) as year,
        month (@dt) as month,
        day (@dt) as day
+
+
